29c977603c077ff834bc3a1787761dd9434a984e
isak wong <isakwong@outlook.com>
2019-04-01 22:26:19 +0800

    修复bug

--- NetRep/Source/NetRep/NetRepGameMode.cpp
+++ NetRep/Source/NetRep/NetRepGameMode.cpp
@@ -8,11 +8,4 @@ ANetRepGameMode::ANetRepGameMode()
 {
 	// use our custom PlayerController class
 	PlayerControllerClass = ANetRepPlayerController::StaticClass();
-
-	// set default pawn class to our Blueprinted character
-	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/TopDownCPP/Blueprints/TopDownCharacter"));
-	if (PlayerPawnBPClass.Class != NULL)
-	{
-		DefaultPawnClass = PlayerPawnBPClass.Class;
-	}
 }
